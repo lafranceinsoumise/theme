fc870d30ccf0c370a56888915f98762f47d12c45
Guillaume Royer <perso@guilro.com>
2016-10-11 18:56:31 +0200

    explanation on index page

--- src/javascripts/components.js
+++ src/javascripts/components.js
@@ -1,13 +1,53 @@
+/* eslint-env browser, jquery */
+
 $('.data-toggle-collapse').attr('data-toggle', 'collapse');
 
-$('.collapse').on('show.bs.collapse', function (event) {
-  var trigger = $('[data-toggle="collapse"][href="#' + event.target.id + '"], [data-toggle="collapse"][data-target="#' + event.target.id + '"]');
+$('.collapse').on('show.bs.collapse', function(event) {
+  var trigger = $('[data-toggle="collapse"][href="#' + event.target.id +
+    '"], [data-toggle="collapse"][data-target="#' + event.target.id + '"]');
   console.log(event);
-  trigger.find('.fa-arrow-circle-o-right').removeClass('fa-arrow-circle-o-right').addClass('fa-arrow-circle-o-down');
+  trigger.find('.fa-arrow-circle-o-right')
+    .removeClass('fa-arrow-circle-o-right')
+    .addClass('fa-arrow-circle-o-down');
 });
 
-$('.collapse').on('hide.bs.collapse', function (event) {
+$('.collapse').on('hide.bs.collapse', function(event) {
   var trigger = $('[data-toggle="collapse"][href="#' + event.target.id + '"],' +
-                         '[data-toggle="collapse"][data-target="#' + event.target.id + '"]');
-  trigger.find('.fa-arrow-circle-o-down').removeClass('fa-arrow-circle-o-down').addClass('fa-arrow-circle-o-right');
+    '[data-toggle="collapse"][data-target="#' + event.target.id + '"]');
+  trigger.find('.fa-arrow-circle-o-down')
+    .removeClass('fa-arrow-circle-o-down')
+    .addClass('fa-arrow-circle-o-right');
+});
+
+// Responsive videos
+
+$(function() {
+  var responsiveVideo = function() {
+    // Find all YouTube videos
+    var $allVideos = $("iframe[src*='//www.youtube.com'], " +
+      "iframe[src*='//www.facebook.com'], " +
+      "iframe[src*='//www.dailymotion.com']");
+    // Figure out and save aspect ratio for each video
+    $allVideos.each(function() {
+      $(this)
+        .data('aspectRatio', this.height / this.width)
+        // and remove the hard coded width/height
+        .removeAttr('height')
+        .removeAttr('width');
+    });
+    // When the window is resized
+    $(window).resize(function() {
+      // Resize all videos according to their own aspect ratio
+      $allVideos.each(function() {
+        var $el = $(this);
+        var newWidth = $el.parent().width();
+
+        $el
+          .width(newWidth)
+          .height(newWidth * $el.data('aspectRatio'));
+      });
+    // Kick off one resize to fix all videos on page load
+    }).resize();
+  };
+  responsiveVideo();
 });
